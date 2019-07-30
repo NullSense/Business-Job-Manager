a9db9bd6530a7757fe05293335b305c96590f71d
nstylo <niklas@stylianou.info>
2019-07-30 19:08:33 +0200

    Define front end routing logic

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,8 +1,31 @@
 import React from 'react';
+import LoginPage from './components/auth/LoginPage';
+import LogoutPage from './components/auth/LogoutPage';
+import RegistrationPage from './components/auth/RegistrationPage';
+import FileUpload from './components/user/FileUpload';
+import TopNav from './components/fixed/TopNav';
+import NotFound from './components/NotFound';
+import MainPage from './components/MainPage';
+import {BrowserRouter as Router, Route, Switch} from 'react-router-dom';
 import './App.css';
 
+/**
+ * return and export this app
+ */
 function App() {
-  return <div>this is working!</div>;
+  return (
+    <Router>
+      <Route path="/" component={TopNav} />
+      <Switch>
+        <Route exact path="/" component={MainPage} />
+        <Route exact path="/login" component={LoginPage} />
+        <Route exact path="/logout" component={LogoutPage} />
+        <Route exact path="/register" component={RegistrationPage} />
+        <Route path="/user/upload" component={FileUpload} />
+        <Route component={NotFound} />
+      </Switch>
+    </Router>
+  );
 }
 
 export default App;
