c1b812c9089d5eb6f4e73929bf8e13c4f2c497f6
Aerial <matas234@gmail.com>
2019-07-31 13:13:49 +0200

    Update configs for moldflow project
    Add .gitignore for Django, static file configuration and a production
    requirements file for deployment to the server.

--- setup.sh
+++ setup.sh
@@ -5,6 +5,6 @@
 # $2 VPS_IP
 
 # rsync built files to the docker build stage
-rsync -ave "ssh -p $0" frontend/build backend "$1"@"$2":/home/"$1"/docker/
+rsync -ave "ssh -p $0" moldflow "$1"@"$2":/home/"$1"/deploy/
 # run docker-compose with nginx
 ssh -p "$0" "$1"@"$2" "cd docker/ && docker-compose down && docker-compose up --build -d"
