7d014434b22bbd15e69d04615287681d7051a8fb
nstylo <niklas@stylianou.info>
2019-07-31 13:32:15 +0200

    Commenting, resolve feedback seen in !1

--- frontend/src/utils/auth_api.js
+++ frontend/src/utils/auth_api.js
@@ -1,11 +1,15 @@
 import axios from 'axios';
 
+// include xsrf token in header
 axios.defaults.xsrfHeaderName = 'X-CSRFTOKEN';
 axios.defaults.xsrfCookieName = 'csrftoken';
 
 // test api url
 const test_api_url = 'https://login.free.beeceptor.com';
 
+/**
+ * takes username and password and makes a login request
+ */
 export async function login(username, password) {
   return await axios
     .post(test_api_url, {
