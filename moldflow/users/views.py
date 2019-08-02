91c123d5f7ce9c3b4684c89e8aa0e58734cc0167
Aerial <matas234@gmail.com>
2019-08-02 13:03:18 +0200

    Change permissions to user view
    Only admin should be able to access the list of users.

--- moldflow/users/views.py
+++ moldflow/users/views.py
@@ -1,4 +1,4 @@
-from rest_framework import viewsets
+from rest_framework import viewsets, permissions
 
 from .serializers import UserSerializer
 from .models import CustomUser
@@ -8,5 +8,7 @@ class UserViewSet(viewsets.ModelViewSet):
     """
     API endpoint for interacting with users
     """
+    # only admin users should be allowed to view the user list
+    permission_classes = [permissions.IsAdminUser]
     queryset = CustomUser.objects.all().order_by('-date_joined')
     serializer_class = UserSerializer
