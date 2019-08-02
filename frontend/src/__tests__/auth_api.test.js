2da042b6607094cf1a5646b0bbea61507abf8dde
nstylo <niklas@stylianou.info>
2019-08-02 13:36:56 +0200

    Remove username

--- frontend/src/__tests__/auth_api.test.js
+++ frontend/src/__tests__/auth_api.test.js
@@ -1,94 +1,102 @@
-import { login, register } from '../components/auth/forms/auth_api';
-import axios from 'axios';
+import { login, register } from "../components/auth/forms/auth_api";
+import axios from "axios";
 // axios mock is defined in __mocks__ in src/ and is automatically handled by jest
 
 // clear .mock.calls after each test
 afterEach(() => {
-  axios.post.mockClear();
+    axios.post.mockClear();
 });
 
-describe('login()', () => {
-  it('should call axios post once', async () => {
-    await login();
-    expect(axios.post.mock.calls.length).toBe(1);
-  });
-
-  it('should read and return empty strings', async () => {
-    await login('', '').then(response => {
-      expect(response.data.email).toBe('');
-      expect(response.data.password).toBe('');
+describe("login()", () => {
+    it("should call axios post once", async () => {
+        await login();
+        expect(axios.post.mock.calls.length).toBe(1);
     });
-  });
 
-  it('should read some string and return it', async () => {
-    await login('someValue', 'someValue').then(response => {
-      expect(response.data.email).toBe('someValue');
-      expect(response.data.password).toBe('someValue');
+    it("should read and return empty strings", async () => {
+        await login("", "").then(response => {
+            expect(response.data.email).toBe("");
+            expect(response.data.password).toBe("");
+        });
     });
-  });
 
-  it('should read null and return empty strings', async () => {
-    await login(null, null).then(response => {
-      expect(response.data.email).toBe('');
-      expect(response.data.password).toBe('');
+    it("should read some string and return it", async () => {
+        await login("someValue", "someValue").then(response => {
+            expect(response.data.email).toBe("someValue");
+            expect(response.data.password).toBe("someValue");
+        });
     });
-  });
 
-  it('should read undefined and return empty strings', async () => {
-    await login(undefined, undefined).then(response => {
-      expect(response.data.email).toBe('');
-      expect(response.data.password).toBe('');
+    it("should read null and return empty strings", async () => {
+        await login(null, null).then(response => {
+            expect(response.data.email).toBe("");
+            expect(response.data.password).toBe("");
+        });
     });
-  });
+
+    it("should read undefined and return empty strings", async () => {
+        await login(undefined, undefined).then(response => {
+            expect(response.data.email).toBe("");
+            expect(response.data.password).toBe("");
+        });
+    });
 });
 
-describe('register()', () => {
-  it('should call axios post once', async () => {
-    await register();
-    expect(axios.post.mock.calls.length).toBe(1);
-  });
-
-  it('should read and return empty strings', async () => {
-    await register('', '', '', '', '', '').then(response => {
-      expect(response.data.email).toBe('');
-      expect(response.data.password).toBe('');
-      expect(response.data.username).toBe('');
-      expect(response.data.phone).toBe('');
-      expect(response.data.address).toBe('');
-      expect(response.data.company).toBe('');
+describe("register()", () => {
+    it("should call axios post once", async () => {
+        await register();
+        expect(axios.post.mock.calls.length).toBe(1);
     });
-  });
 
-  it('should read some string and return it', async () => {
-    await register('someValue', 'someValue', 'someValue', 'someValue', 'someValue', 'someValue').then(response => {
-      expect(response.data.email).toBe('someValue');
-      expect(response.data.password).toBe('someValue');
-      expect(response.data.username).toBe('someValue');
-      expect(response.data.phone).toBe('someValue');
-      expect(response.data.address).toBe('someValue');
-      expect(response.data.company).toBe('someValue');
+    it("should read and return empty strings", async () => {
+        await register("", "", "", "", "").then(response => {
+            expect(response.data.email).toBe("");
+            expect(response.data.password).toBe("");
+            expect(response.data.phone).toBe("");
+            expect(response.data.address).toBe("");
+            expect(response.data.company).toBe("");
+        });
     });
-  });
 
-  it('should read null and return empty strings', async () => {
-    await register(null, null, null, null, null, null).then(response => {
-      expect(response.data.email).toBe('');
-      expect(response.data.password).toBe('');
-      expect(response.data.username).toBe('');
-      expect(response.data.phone).toBe('');
-      expect(response.data.address).toBe('');
-      expect(response.data.company).toBe('');
+    it("should read some string and return it", async () => {
+        await register(
+            "someValue",
+            "someValue",
+            "someValue",
+            "someValue",
+            "someValue"
+        ).then(response => {
+            expect(response.data.email).toBe("someValue");
+            expect(response.data.password).toBe("someValue");
+            expect(response.data.phone).toBe("someValue");
+            expect(response.data.address).toBe("someValue");
+            expect(response.data.company).toBe("someValue");
+        });
     });
-  });
 
-  it('should read undefined and return empty strings', async () => {
-    await register(undefined, undefined, undefined, undefined, undefined, undefined).then(response => {
-      expect(response.data.email).toBe('');
-      expect(response.data.password).toBe('');
-      expect(response.data.username).toBe('');
-      expect(response.data.phone).toBe('');
-      expect(response.data.address).toBe('');
-      expect(response.data.company).toBe('');
+    it("should read null and return empty strings", async () => {
+        await register(null, null, null, null, null).then(response => {
+            expect(response.data.email).toBe("");
+            expect(response.data.password).toBe("");
+            expect(response.data.phone).toBe("");
+            expect(response.data.address).toBe("");
+            expect(response.data.company).toBe("");
+        });
     });
-  });
+
+    it("should read undefined and return empty strings", async () => {
+        await register(
+            undefined,
+            undefined,
+            undefined,
+            undefined,
+            undefined
+        ).then(response => {
+            expect(response.data.email).toBe("");
+            expect(response.data.password).toBe("");
+            expect(response.data.phone).toBe("");
+            expect(response.data.address).toBe("");
+            expect(response.data.company).toBe("");
+        });
+    });
 });
--- frontend/src/components/auth/forms/auth_api.js
+++ frontend/src/components/auth/forms/auth_api.js
@@ -1,65 +1,65 @@
-import axios from 'axios';
+import axios from "axios";
 
 // include xsrf token in header
-axios.defaults.xsrfHeaderName = 'X-CSRFTOKEN';
-axios.defaults.xsrfCookieName = 'csrftoken';
-axios.defaults.headers.common['x-api-key'] = 'e8f411d2de5d42c485df61bc8a8ff977';
+axios.defaults.xsrfHeaderName = "X-CSRFTOKEN";
+axios.defaults.xsrfCookieName = "csrftoken";
+axios.defaults.headers.common["x-api-key"] = "e8f411d2de5d42c485df61bc8a8ff977";
 
 // postman mock api
-const test_api_url = 'https://59a86326-905c-47d9-a33e-383906e3685e.mock.pstmn.io';
+const test_api_url =
+    "https://59a86326-905c-47d9-a33e-383906e3685e.mock.pstmn.io";
 
 /**
  * takes email and password and makes a login request
  */
 export async function login(email, password) {
-  return await axios
-    .post(test_api_url + '/login/', {
-      email: email || '',
-      password: password || ''
-    })
-    .then(response => {
-      return response;
-    })
-    .catch(err => {
-      return err.response;
-    });
+    return await axios
+        .post(test_api_url + "/login/", {
+            email: email || "",
+            password: password || ""
+        })
+        .then(response => {
+            return response;
+        })
+        .catch(err => {
+            return err.response;
+        });
 }
 
 /**
  * takes user credentials and makes a registration request
  */
-export async function register(email, username, password, phone, address, company) {
-  return await axios
-    .post(test_api_url + '/register/', {
-      email: email || '',
-      username: username || '',
-      password: password || '',
-      phone: phone || '',
-      address: address || '',
-      company: company || ''
-    })
-    .then(response => {
-      return response;
-    })
-    .catch(err => {
-      return err.response;
-    });
+export async function register(email, password, phone, address, company) {
+    return await axios
+        .post(test_api_url + "/register/", {
+            email: email || "",
+            password: password || "",
+            phone: phone || "",
+            address: address || "",
+            company: company || ""
+        })
+        .then(response => {
+            return response;
+        })
+        .catch(err => {
+            return err.response;
+        });
 }
 
 /**
  * takes email to request a password reset
  */
 export async function requestReset(email) {
-  return await axios
-    .post(test_api_url + '/request-reset/', {
-      email: email || ''
-    })
-    .then(response => {
-      return response;
-    })
-    .catch(err => {
-      return err.response;
-    });
+    return await axios
+        .post(test_api_url + "/request-reset/", {
+            email: email || ""
+        })
+        .then(response => {
+            return response;
+        })
+        .catch(err => {
+            return err.response;
+        });
 }
 
 /**
@@ -67,14 +67,14 @@ export async function requestReset(email) {
  * TODO: how to identify which password to update?
  */
 export async function reset(password) {
-  return await axios
-    .put(test_api_url + '/reset/', {
-      email: password || ''
-    })
-    .then(response => {
-      return response;
-    })
-    .catch(err => {
-      return err.response;
-    });
+    return await axios
+        .put(test_api_url + "/reset/", {
+            email: password || ""
+        })
+        .then(response => {
+            return response;
+        })
+        .catch(err => {
+            return err.response;
+        });
 }
