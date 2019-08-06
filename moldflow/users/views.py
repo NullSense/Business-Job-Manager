dac69ccfeeb34970b3b5e562ccae064c83e54ce3
Aerial <matas234@gmail.com>
2019-08-06 20:24:43 +0200

    Rework UserViewSet permissions
    Used to be able to register users through UserViewSet, now disabled.
    Fixed old IsLoggedInUserOrAdmin permissions.

--- moldflow/users/views.py
+++ moldflow/users/views.py
@@ -1,7 +1,8 @@
 from rest_framework import permissions, viewsets
+from rest_framework.response import Response
 
 from .models import CustomUser
-from .permissions import IsLoggedInUserOrAdmin
+from .permissions import IsLoggedInUserOrStaff, IsStaffUser
 from .serializers import UserSerializer
 
 
@@ -15,14 +16,25 @@ class UserViewSet(viewsets.ModelViewSet):
     queryset = CustomUser.objects.all().order_by("-date_joined")
     serializer_class = UserSerializer
 
+    def create(self, request):
+        """
+        We don't allow creation through this view,
+        use /api/auth/register/ instead
+        """
+        return Response(
+            "This endpoint does not allow for registration, use /api/auth/register/ instead.",
+            status=405,
+        )
+
     def get_permissions(self):
         """
         Enforce different permissions per request types
         """
         permission_classes = []
-        # anyone is able to register
+        # noone should be able to post to this view
+        # (registration is done with DRR)
         if self.action == "create":
-            permission_classes = [permissions.AllowAny]
+            permission_classes = []
         # only the logged in user or a staff member should be able to:
         # get, update their credentials
         elif (
@@ -30,8 +42,8 @@ class UserViewSet(viewsets.ModelViewSet):
             or self.action == "update"
             or self.action == "partial_update"
         ):
-            permission_classes = [IsLoggedInUserOrAdmin]
-        # only the admin can view all users or remove users
+            permission_classes = [IsLoggedInUserOrStaff]
+        # only the admin can list all users or remove users
         elif self.action == "list" or self.action == "destroy":
-            permission_classes = [permissions.IsAdminUser]
+            permission_classes = [IsStaffUser]
         return [permission() for permission in permission_classes]
