333c2dcb601b301c2e92216fbd3d103b3fd3ae5f
Aerial <matas234@gmail.com>
2019-08-08 20:12:27 +0200

    Add django jet customization

--- moldflow/moldflow/settings.py
+++ moldflow/moldflow/settings.py
@@ -40,7 +40,7 @@ if DJANGO_ENV == "development":
     EMAIL_PORT = "1025"
     EMAIL_HOST_USER = "test@gmail.com"
     EMAIL_HOST_PASSWORD = None
-    EMAIL_BACKEND = 'django.core.mail.backends.console.EmailBackend'
+    EMAIL_BACKEND = "django.core.mail.backends.console.EmailBackend"
 else:  # deployment env
     DEBUG = False
     ALLOWED_HOSTS = [os.getenv("HOST")]
@@ -52,7 +52,7 @@ else:  # deployment env
     EMAIL_USE_TLS = True
     EMAIL_HOST_USER = os.getenv("EMAIL_HOST_USER")
     EMAIL_HOST_PASSWORD = os.getenv("EMAIL_HOST_PASSWORD")
-    EMAIL_BACKEND = 'django.core.mail.backends.smtp.EmailBackend'
+    EMAIL_BACKEND = "django.core.mail.backends.smtp.EmailBackend"
 
 CSRF_COOKIE_NAME = "csrftoken"
 
@@ -194,6 +194,31 @@ REST_REGISTRATION = {
         "date_joined",
     ),
 }
+
+JET_DEFAULT_THEME = "light-gray"
+JET_THEMES = [
+    {
+        "theme": "default",  # theme folder name
+        "color": "#47bac1",  # color of the theme's button in user menu
+        "title": "Default",  # theme title
+    },
+    {"theme": "green", "color": "#44b78b", "title": "Green"},
+    {"theme": "light-green", "color": "#2faa60", "title": "Light Green"},
+    {"theme": "light-violet", "color": "#a464c4", "title": "Light Violet"},
+    {"theme": "light-blue", "color": "#5EADDE", "title": "Light Blue"},
+    {"theme": "light-gray", "color": "#222", "title": "Light Gray"},
+]
+
+JET_SIDE_MENU_ITEMS = [
+    {"app_label": "jobs", "items": [{"name": "job"}]},
+    {"app_label": "users", "items": [{"name": "customuser"}]},
+    {
+        "label": "GitLab repository",
+        "url": "https://gitlab.com/MrCappuccino/mod",
+        "url_blank": True,
+    },
+]
+
 
 # Internationalization
 # https://docs.djangoproject.com/en/2.2/topics/i18n/
