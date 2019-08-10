0192d6f06b23610efa1f20f3458d2c3e759b9dd5
nstylo <niklas@stylianou.info>
2019-08-10 19:13:24 +0200

    Add user project page

--- frontend/src/components/pages/user/UserProjectPage.js
+++ frontend/src/components/pages/user/UserProjectPage.js
@@ -10,18 +10,18 @@ export default class UserProjectPage extends Component {
       {
         title: 'Title',
         dataIndex: 'name',
-        key: 'name',
-        render: text => <a href="javascript:;">{text}</a>
+        key: 'name'
       },
       {
         title: 'Description',
-        dataIndex: 'desc',
-        key: 'desc'
+        dataIndex: 'description',
+        key: 'description'
       },
       {
         title: 'File',
         dataIndex: 'file',
-        key: 'file'
+        key: 'file',
+        render: text => <a href={text.input}>{text}</a>
       },
       {
         title: 'Created',
@@ -31,15 +31,30 @@ export default class UserProjectPage extends Component {
       {
         title: 'Action',
         key: 'action',
-        render: (text, record) => <span>Download as PDF</span>
+        render: (text, record) => <span>Download as PDF</span> // TODO
       }
     ]
   };
 
+  async componentDidMount() {
+    // fetch initial data from the first page
+    const data = await this.getData('/jobs/?page=1');
+    // update state
+    this.setData(data);
+  }
+
+  /**
+   * fetches the job-data from the server
+   * @param {string} url file resource link
+   */
   getData = async url => {
     return await get(url).then(response => response.data);
   };
 
+  /**
+   * calls the parsing function on data and updates the state
+   * @param {array} data userjobs
+   */
   setData = data => {
     const parsedData = this.parseData(data);
     this.setState({
@@ -48,6 +63,10 @@ export default class UserProjectPage extends Component {
     });
   };
 
+  /**
+   * parses the the raw api data
+   * @param {array} data userjobs
+   */
   parseData = data => {
     return {
       total: data.count,
@@ -56,25 +75,55 @@ export default class UserProjectPage extends Component {
           key: item.url,
           name: item.name,
           description: item.description,
-          created: item.created, // TODO: parse datetime
+          created: this.parseDateTime(item.created),
           estimated: item.estimated,
-          file: item.project,
-          result: item.result,
-          filename: null // TODO: parse file name
+          file: this.parseFileName(item.project), // TODO: parse file name
+          result: item.result
         };
       })
     };
   };
 
+  /**
+   * reads filename from server as url and parses into filename
+   * @param {string} url to parse url
+   */
+  parseFileName = url => {
+    return url.match(/[\w\-. ]*$/);
+  };
+
+  /**
+   * Format dateTime for better readability
+   * @param {string} dateTime the timestamp of the item
+   */
+  parseDateTime = dateTime => {
+    let time = new Date(dateTime);
+    let hours = time.getHours();
+    let minutes = time.getMinutes();
+
+    if (minutes < 10) {
+      minutes = '0' + minutes;
+    }
+
+    let suffix = 'AM';
+    if (hours >= 12) {
+      suffix = 'PM';
+      hours = hours - 12;
+    }
+    if (hours === 0) {
+      hours = 12;
+    }
+
+    return time.toDateString() + ' - ' + hours + ':' + minutes + ' ' + suffix;
+  };
+
+  /**
+   * handles pagination, calls data getter and updates local state
+   */
   handleChange = async (page, pageSize) => {
     const data = await this.getData(`/jobs/?page=${page}`);
     this.setData(data);
   };
-
-  async componentDidMount() {
-    const data = await this.getData('/jobs/?page=1');
-    this.setData(data);
-  }
 
   render() {
     return (
