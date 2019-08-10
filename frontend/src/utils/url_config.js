3a74936a0523e263755e8b5b8812200a43aecc49
niklas stylianou <niklas@stylianou.info>
2019-08-10 06:42:38 +0000

    Make url config more compact

--- frontend/src/utils/url_config.js
+++ frontend/src/utils/url_config.js
@@ -1,12 +1,9 @@
 let BACKEND_URL;
 const hostname = window && window.location && window.location.hostname;
 
-if (hostname === 'moldflow.ml') {
-  // production
-  BACKEND_URL = 'https://moldflow.ml';
-} else if (hostname === 'mold.ga') {
-  // staging
-  BACKEND_URL = 'https://mold.ga';
+if (hostname === 'moldflow.ml' || hostname === 'mold.ga') {
+  // production + staging
+  BACKEND_URL = `https://${hostname}`;
 } else {
   // development
   BACKEND_URL = process.env.REACT_APP_BACKEND_URL || 'http://127.0.0.1:8000';
