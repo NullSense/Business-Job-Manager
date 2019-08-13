596c93a84a018f73ab2e01bc96a19dff541ee9aa
nstylo <niklas@stylianou.info>
2019-08-13 18:08:50 +0200

    Fix missing return in isAuthenticated + make async

--- frontend/src/utils/requests.js
+++ frontend/src/utils/requests.js
@@ -62,8 +62,8 @@ export async function get(url, config = {}) {
  * queries the backend if user authenticated
  * @return { boolean } whether user is logged in
  */
-export function isAuthenticated() {
-  get('/auth/logout/').then(response => {
+export async function isAuthenticated() {
+  return await get('/auth/logout/').then(response => {
     if (response.status === 405) {
       return true;
     } else {
