90440447b20aaf4cc02bef0eb098070af55dc3d5
Aerial <matas234@gmail.com>
2019-08-17 19:33:08 +0200

    Refactor emails

--- moldflow/jobs/emails.py
+++ moldflow/jobs/emails.py
@@ -8,12 +8,39 @@ class EmailJob(EmailMessage):
     def __init__(self, job, subject="", body="", receiver=["receiver@mail.com"]):
         self.subject = subject
         self.plain_body = body
+        self.html_body = body
         self.sender = [settings.EMAIL_HOST_USER]
         self.receiver = receiver
         self.job = job
 
+    def _get_recipients(self):
+        return self.receiver
+
+    def _get_subject(self):
+        return self.subject
+
+    def _get_body(self):
+        return self.plain_body
+
     def send_mail(self):
-        send_mail(self.subject, self.plain_body, self.sender, self.receiver)
+        """
+        Send an email to the client stating that their project
+        results have been uploaded, only if the client is active
+        """
+        if self._get_recipients():
+            self._get_subject()
+            self._get_body()
+            send_mail(
+                self.subject,
+                self.plain_body,
+                self.sender,
+                self.receiver,
+                html_message=self.html_body,
+            )
+        else:
+            raise ValueError(
+                "The receiver of the email either does not exist or is not active"
+            )
 
 
 class EmailJobStaff(EmailJob):
@@ -21,7 +48,7 @@ class EmailJobStaff(EmailJob):
     Handles sending staff emails when a new job gets created
     """
 
-    def _compose_subject_staff(self):
+    def _get_subject(self):
         """
         Compose the job email subject for staff
         """
@@ -29,7 +56,7 @@ class EmailJobStaff(EmailJob):
             self.job.owner.company
         )
 
-    def _compose_body_staff(self):
+    def _get_body(self):
         """
         The body of the email for sending out a
         notification that a job has been added to staff
@@ -55,25 +82,18 @@ class EmailJobStaff(EmailJob):
         # alternative email for those that cannot render html
         self.plain_body = render_to_string("new_job_email.txt", context)
 
-    def _get_staff(self):
+    def _get_recipients(self):
         """
         Set the mail receivers to all staff members that are active
         """
-        self.receiver = self.job.owner.__class__.objects.filter(
+        receiver = self.job.owner.__class__.objects.filter(
             is_staff=True, is_active=True
         ).values_list("email", flat=True)
 
-    def send_mail(self):
-        self._get_staff()
-        self._compose_subject_staff()
-        self._compose_body_staff()
-        send_mail(
-            self.subject,
-            self.plain_body,
-            self.sender,
-            self.receiver,
-            html_message=self.html_body,
-        )
+        if receiver is not None and receiver is not False:
+            self.receiver = receiver
+            return True
+        return False
 
 
 class EmailJobClient(EmailJob):
@@ -81,17 +101,11 @@ class EmailJobClient(EmailJob):
     Handles sending client emails when a new job gets created
     """
 
-    def __init__(
-        self, job, update=False, subject="", body="", receiver=["receiver@mail.com"]
-    ):
+    def __init__(self, update=False, *args, **kwargs):
         self.update = update
-        self.subject = subject
-        self.plain_body = body
-        self.sender = [settings.EMAIL_HOST_USER]
-        self.receiver = receiver
-        self.job = job
+        super().__init__(*args, **kwargs)
 
-    def _compose_subject_client(self):
+    def _get_subject(self):
         """
         Compose the job email subject for client
         """
@@ -100,7 +114,7 @@ class EmailJobClient(EmailJob):
         else:
             self.subject = 'Your job "{0}" results have been uploaded!'.format(self.job.name)
 
-    def _compose_body_client(self):
+    def _get_body(self):
         """
         The body of the email for sending out a
         notification that a job has been added to client
@@ -116,34 +130,16 @@ class EmailJobClient(EmailJob):
         # alternative email for those that cannot render html
         self.plain_body = render_to_string("job_results_email.txt", context)
 
-    def _set_receiver(self):
+    def _get_recipients(self):
         """
         Get clients that exist and are active
         """
         receiver = [self.job.owner.email]
         active = self.job.owner.is_active
+
         if receiver is not None and active:
             self.receiver = receiver
             return True
         else:
             return False
 
-    def send_mail(self):
-        """
-        Send an email to the client stating that their project
-        results have been uploaded, only if the client is active
-        """
-        if self._set_receiver():
-            self._compose_subject_client()
-            self._compose_body_client()
-            send_mail(
-                self.subject,
-                self.plain_body,
-                self.sender,
-                self.receiver,
-                html_message=self.html_body,
-            )
-        else:
-            raise ValueError(
-                "The receiver of the email either does not exist or is not active"
-            )
