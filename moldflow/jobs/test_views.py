23f5cc2da05e8e28efbd359bd6c71e2cf19ccf2a
Aerial <matas234@gmail.com>
2019-08-18 13:15:21 +0200

    Update Job unit tests

--- moldflow/jobs/test_views.py
+++ moldflow/jobs/test_views.py
@@ -40,9 +40,8 @@ class TestJobViewNonStaff(APITestCase):
         self.client.force_authenticate(self.user)
 
     def test_get_list_empty(self):
-        user1 = mixer.blend(
-            "users.CustomUser", is_active=True, phone="+31230802611"
-        )
+        user1 = mixer.blend("users.CustomUser",
+                            is_active=True, phone="+31230802611")
         mixer.blend("jobs.Job", owner=user1)
         resp = self.client.get("/api/jobs/")
 
@@ -50,16 +49,17 @@ class TestJobViewNonStaff(APITestCase):
         assert resp.data["count"] == 0, "The user didn't create any of his own jobs"
 
     def test_get_list_created(self):
-        user1 = mixer.blend(
-            "users.CustomUser", is_active=True, phone="+31230802611"
-        )
+        user1 = mixer.blend("users.CustomUser",
+                            is_active=True, phone="+31230802611")
         mixer.blend("jobs.Job", owner=user1)
         mixer.blend("jobs.Job", owner=self.user)
         resp = self.client.get("/api/jobs/")
 
         assert resp.status_code == 200, "The user should see their own job list"
         assert resp.data["count"] == 1, "The user didn't create any of his own jobs"
-        # TODO: verify that the correct user gets associated with the job
+        assert (
+            str(self.user.pk) in resp.data["results"][0]["owner"]
+        ), "The owner of the job should be associated with it"
 
     def test_post(self):
         job_name = "New job name"
@@ -78,12 +78,15 @@ class TestJobViewNonStaff(APITestCase):
         # assert resp.data.get("url") == "http://testserver/api/jobs/{0}/".format(
         # self.user.pk
         # )
-        # assert resp.data.get("owner") == self.user.company
+        print(resp.data)
         assert resp.data.get("name") == job_name
         assert resp.data.get("description") == job_description
         assert resp.data.get("estimated") is None
         assert resp.data.get("progress") == 0
         assert resp.data.get("result") is None
+        assert resp.data.get("created") is not None
+        assert resp.data.get("created")
+        assert str(self.user.pk) in resp.data.get("owner")
         # TODO: mock the url to test it and 'created'
 
     def test_delete(self):
@@ -105,19 +108,19 @@ class TestJobViewStaff(APITestCase):
         self.client.force_authenticate(self.user)
 
     def test_get_list(self):
-        user1 = mixer.blend(
-            "users.CustomUser", is_active=True, phone="+31230802611"
-        )
+        user1 = mixer.blend("users.CustomUser",
+                            is_active=True, phone="+31230802611")
         mixer.blend("jobs.Job", owner=user1)
         resp = self.client.get("/api/jobs/")
 
-        assert resp.status_code == 200, "The staff user should be able to see other jobs."
+        assert (
+            resp.status_code == 200
+        ), "The staff user should be able to see other jobs."
         assert resp.data["count"] == 1, "The staff user sees other jobs"
 
     def test_get_list_created(self):
-        user1 = mixer.blend(
-            "users.CustomUser", is_active=True, phone="+31230802611"
-        )
+        user1 = mixer.blend("users.CustomUser",
+                            is_active=True, phone="+31230802611")
         mixer.blend("jobs.Job", owner=user1)
         mixer.blend("jobs.Job", owner=self.user)
         resp = self.client.get("/api/jobs/")
@@ -152,9 +155,8 @@ class TestJobViewStaff(APITestCase):
         # TODO: mock the url to test it and 'created'
 
     def test_delete(self):
-        user1 = mixer.blend(
-            "users.CustomUser", is_active=True, phone="+31230802611"
-        )
+        user1 = mixer.blend("users.CustomUser",
+                            is_active=True, phone="+31230802611")
         job1 = mixer.blend("jobs.Job", owner=user1)
 
         resp = self.client.delete("/api/jobs/{0}/".format(job1.pk))
