3e082e4ec650234d0bb36e1f29f06b898b8ed822
nstylo <niklas@stylianou.info>
2019-08-18 14:55:46 +0200

    Refactor url_config
    This way only local environments are explicitly specified, else on each
    other host ( i.e. staging or deployment ) will have standard behavior

---
+++ frontend/src/__tests__/url_config.test.js
@@ -0,0 +1,61 @@
+import { localHosts } from '../utils/url_config';
+const location = window.location; // keep backup of global object
+
+beforeEach(() => {
+  delete window.location; // delete global object in order to override
+  jest.resetModules(); // reset imports
+});
+
+afterEach(() => {
+  window.location = location; // reset global object
+});
+
+it('should set the correct url on hostname "moldflow.ml"', () => {
+  window.location = { hostname: 'moldflow.ml' };
+
+  const { API_URL } = require('../utils/url_config');
+
+  expect(API_URL).toBe(`https://${window.location.hostname}/api`);
+});
+
+it('should set the correct url on hostname "mold.ga"', () => {
+  window.location = { hostname: 'mold.ga' };
+
+  const { API_URL } = require('../utils/url_config');
+
+  expect(API_URL).toBe(`https://${window.location.hostname}/api`);
+});
+
+it('should set the correct url on any other deployment env', () => {
+  window.location = { hostname: 'test.test' };
+
+  const { API_URL } = require('../utils/url_config');
+
+  expect(API_URL).toBe(`https://${window.location.hostname}/api`);
+});
+
+it('should prefer env var on local environments', () => {
+  process.env.REACT_APP_BACKEND_URL = 'http://whatever:9999'; // override env var
+
+  // test for all local hosts
+  localHosts.forEach(hostname => {
+    window.location = { hostname };
+
+    const { API_URL } = require('../utils/url_config');
+
+    expect(API_URL).toBe(`${process.env.REACT_APP_BACKEND_URL}/api`);
+  });
+});
+
+it('should fall back to 127.0.0.1:8000 if local and no env var set', () => {
+  delete process.env.REACT_APP_BACKEND_URL; // if env var not declared
+
+  // test for all local hosts
+  localHosts.forEach(hostname => {
+    window.location = { hostname };
+
+    const { API_URL } = require('../utils/url_config');
+
+    expect(API_URL).toBe(`http://127.0.0.1:8000/api`);
+  });
+});
--- frontend/src/utils/url_config.js
+++ frontend/src/utils/url_config.js
@@ -1,12 +1,16 @@
 let BACKEND_URL;
 const hostname = window && window.location && window.location.hostname;
 
-if (hostname === 'moldflow.ml' || hostname === 'mold.ga') {
-  // production + staging
-  BACKEND_URL = `https://${hostname}`;
-} else {
+export const localHosts = ['localhost', '127.0.0.1'];
+
+if (localHosts.includes(hostname)) {
   // development
+  // careful: env vars do parse to strings, so initializing to null will yield
+  // a string which is NOT falsely
   BACKEND_URL = process.env.REACT_APP_BACKEND_URL || 'http://127.0.0.1:8000';
+} else {
+  // production and staging
+  BACKEND_URL = `https://${hostname}`;
 }
 
 export const API_URL = `${BACKEND_URL}/api`;
