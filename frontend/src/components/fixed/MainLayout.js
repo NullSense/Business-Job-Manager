97b24043b2f144bec6d4348168df43ec4953070f
nstylo <niklas@stylianou.info>
2019-08-20 18:53:38 +0200

    First draft main navigation bar

--- frontend/src/components/fixed/MainLayout.js
+++ frontend/src/components/fixed/MainLayout.js
@@ -1,30 +1,24 @@
-import React from 'react';
-import { Layout, Menu, Breadcrumb } from 'antd';
+import React, { useContext } from 'react';
+import { AuthContext } from '../../App';
+import { Link } from 'react-router-dom';
+import { Layout, Menu, Divider, Input } from 'antd';
+import logo from '../../res/codeps.png';
 
 const { Header, Content, Footer } = Layout;
+const { Search } = Input;
 
 export default props => {
   return (
-    <Layout style={{ height: '100vh' }}>
-      <Header className="header">
-        <div className="logo" />
-        <Menu
-          theme="dark"
-          mode="horizontal"
-          defaultSelectedKeys={['2']}
-          style={{ lineHeight: '64px' }}
-        >
-          <Menu.Item key="1">nav 1</Menu.Item>
-          <Menu.Item key="2">nav 2</Menu.Item>
-          <Menu.Item key="3">nav 3</Menu.Item>
-        </Menu>
+    <Layout>
+      <Header>
+        <div className="logo-wrapper">
+          <span className="helper"></span>
+          <img className="logo-main" src={logo} alt="codeps logo" />
+        </div>
+        <Utils />
+        <Sections />
       </Header>
       <Content style={{ padding: '0 50px', height: '100vh' }}>
-        <Breadcrumb style={{ margin: '16px 0' }}>
-          <Breadcrumb.Item>Home</Breadcrumb.Item>
-          <Breadcrumb.Item>List</Breadcrumb.Item>
-          <Breadcrumb.Item>App</Breadcrumb.Item>
-        </Breadcrumb>
         <Layout style={{ padding: '24px 0', background: '#fff' }}>
           <Content style={{ padding: '0 24px', height: '100%' }}>
             {props.children}
@@ -33,5 +27,59 @@ export default props => {
       </Content>
       <Footer style={{ textAlign: 'center' }}>Moldflow.ml ©2019</Footer>
     </Layout>
+  );
+};
+
+const Sections = () => {
+  return (
+    <Menu
+      className="sections"
+      theme="dark"
+      mode="horizontal"
+      defaultSelectedKeys={['2']}
+    >
+      <Menu.Item className="section-item" key="1">
+        <Link to="/">Home</Link>
+      </Menu.Item>
+      <Menu.Item className="section-item" key="2">
+        <Link to="/">Solutions</Link>
+      </Menu.Item>
+      <Menu.Item className="section-item" key="3">
+        <Link to="/">About</Link>
+      </Menu.Item>
+      <Menu.Item className="section-item" key="4">
+        <Link to="/">Contact</Link>
+      </Menu.Item>
+    </Menu>
+  );
+};
+
+const Utils = () => {
+  const { isAuthenticated } = useContext(AuthContext);
+
+  return (
+    <div className="utils">
+      <div className="util-item">
+        {isAuthenticated === true ? (
+          <Link to="/user">My Account</Link>
+        ) : (
+          <>
+            <Link to="/auth/register">register</Link>
+            <Divider type="vertical" />
+            <Link to="/auth/login">login</Link>
+          </>
+        )}
+        <Divider type="vertical" />
+      </div>
+      <div className="util-item">
+        <Search
+          placeholder="search"
+          enterButton="Go"
+          size="small"
+          style={{ width: 200 }}
+          onSearch={value => console.log(value)}
+        />
+      </div>
+    </div>
   );
 };
