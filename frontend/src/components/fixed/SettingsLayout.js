5e7a30fe63a647832dfab46de075402c9cf1fe4c
nstylo <niklas@stylianou.info>
2019-08-25 21:01:30 +0200

    Add settings routing

---
+++ frontend/src/components/fixed/SettingsLayout.js
@@ -0,0 +1,53 @@
+import React from 'react';
+import history from '../../history';
+import RoutingMenuItem from '../other/RoutingMenuItem';
+import { Layout, Menu, Icon } from 'antd';
+
+const { Sider, Content } = Layout;
+
+export default props => {
+  // extract last word of url, if valid menu item key => highlight
+  const pathKey = history.location.pathname.replace(/\/$/, '').match(/\w*$/)[0];
+
+  return (
+    <Layout style={{ height: '100vh' }}>
+      <Content>
+        <Layout
+          style={{ padding: '24px 0', background: '#fff', height: '100%' }}
+        >
+          <Sider width={200} style={{ background: '#fff' }}>
+            <Settings pathKey={pathKey} />
+          </Sider>
+          <Content style={{ padding: '0 24px', minHeight: 280 }}>
+            {props.children}
+          </Content>
+        </Layout>
+      </Content>
+    </Layout>
+  );
+};
+
+const Settings = props => {
+  const { pathKey } = props;
+
+  return (
+    <Menu
+      mode="inline"
+      style={{ height: '100%' }}
+      defaultSelectedKeys={[pathKey]}
+    >
+      <RoutingMenuItem key="account" to="/user/settings/account">
+        <Icon type="file-add" theme="twoTone" />
+        Account
+      </RoutingMenuItem>
+      <RoutingMenuItem key="profile" to="/user/settings/profile">
+        <Icon type="file-add" theme="twoTone" />
+        Profile
+      </RoutingMenuItem>
+      <RoutingMenuItem key="billing" to="/user/settings/billing">
+        <Icon type="file-add" theme="twoTone" />
+        Billing
+      </RoutingMenuItem>
+    </Menu>
+  );
+};
